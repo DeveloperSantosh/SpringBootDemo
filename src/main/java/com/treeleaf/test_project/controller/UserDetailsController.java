0d3fbd1c581971667d11b7d24efc277e0f79073a
Santosh <coder.santosh2056@gmail.com>
2022-02-28 16:15:37 +0545

    "Liquibase and Redis Implementation"

--- src/main/java/com/treeleaf/test_project/controller/UserDetailsController.java
+++ src/main/java/com/treeleaf/test_project/controller/UserDetailsController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
@@ -16,8 +15,8 @@ public class UserDetailsController {
     UserDetailsService userDetailsService;
 
     @GetMapping("/userDetails")
-    public List<UserDetails> getAllInvoices(){
-        return userDetailsService.getAllUserDetails();
+    public ResponseEntity<List<UserDetails>> getAllUserDetails(){
+        return ResponseEntity.ok(userDetailsService.getAllUserDetails());
     }
 
 }
